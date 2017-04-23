b3b421d5c084a197bbcb4dcaa686daaddeab6785
lilelife <1663595737@qq.com>
2017-04-23 17:33:42 +0800

    ll

---
+++ app/src/main/java/com/example/lile/localvisual/MainActivity.java
@@ -0,0 +1,236 @@
+package com.example.lile.localvisual;
+
+import android.app.Activity;
+import android.app.Service;
+import android.os.Bundle;
+import android.os.Vibrator;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.baidu.location.BDLocation;
+import com.baidu.location.BDLocationListener;
+import com.baidu.location.BDNotifyListener;
+import com.baidu.location.LocationClient;
+import com.baidu.location.LocationClientOption;
+import com.baidu.mapapi.SDKInitializer;
+import com.baidu.mapapi.map.BaiduMap;
+import com.baidu.mapapi.map.BitmapDescriptor;
+import com.baidu.mapapi.map.MapStatusUpdate;
+import com.baidu.mapapi.map.MapStatusUpdateFactory;
+import com.baidu.mapapi.map.MapView;
+import com.baidu.mapapi.map.MyLocationConfiguration;
+import com.baidu.mapapi.map.MyLocationData;
+import com.baidu.mapapi.model.LatLng;
+import com.example.lile.localvisual.view.LocationActivity;
+
+public class MainActivity extends Activity {
+    private MapView mapView;
+    private BaiduMap bdMap;
+
+    private LocationClient locationClient;
+    private BDLocationListener locationListener;
+    private BDNotifyListener notifyListener;
+
+    private double longitude;// 精度
+    private double latitude;// 维度
+    private float radius;// 定位精度半径，单位是米
+    private String addrStr;// 反地理编码
+    private String province;// 省份信息
+    private String city;// 城市信息
+    private String district;// 区县信息
+    private float direction;// 手机方向信息
+
+    private int locType;
+
+    // 定位按钮
+    private Button locateBtn;
+    private Button btn_search;//搜索按钮
+    // 定位模式 （普通-跟随-罗盘）
+    private MyLocationConfiguration.LocationMode currentMode;
+    // 定位图标描述
+    private BitmapDescriptor currentMarker = null;
+    // 记录是否第一次定位
+    private boolean isFirstLoc = true;
+
+    //振动器设备
+    private Vibrator mVibrator;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        SDKInitializer.initialize(getApplicationContext());
+        setContentView(R.layout.activity_main);
+        mapView = (MapView) findViewById(R.id.baidumap);
+
+    }
+    View.OnClickListener listener=new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()) {
+                case R.id.btn_location:// 定位
+                    switch (currentMode) {
+                        case FOLLOWING:
+                            locateBtn.setText("跟随");
+                            currentMode = MyLocationConfiguration.LocationMode.FOLLOWING;
+                            break;
+                        case COMPASS:
+                            locateBtn.setText("罗盘");
+                            currentMode = MyLocationConfiguration.LocationMode.COMPASS;
+                            break;
+                        case NORMAL:
+                            locateBtn.setText("普通");
+                            currentMode = MyLocationConfiguration.LocationMode.NORMAL;
+                            break;
+                    }
+                    bdMap.setMyLocationConfigeration(new MyLocationConfiguration(
+                            currentMode, true, currentMarker));
+                    break;
+            }
+        }
+    };
+    void init() {
+        mapView = (MapView) findViewById(R.id.bd_mapview);
+        bdMap = mapView.getMap();
+        locateBtn = (Button) findViewById(R.id.locate_btn);
+        locateBtn.setOnClickListener(listener);
+        currentMode = MyLocationConfiguration.LocationMode.COMPASS;
+        locateBtn.setText("罗盘");
+        mVibrator =(Vibrator)getApplicationContext().getSystemService(Service.VIBRATOR_SERVICE );
+        bdMap.setMyLocationEnabled(true);
+        // 1. 初始化LocationClient类
+        locationClient = new LocationClient(getApplicationContext());
+        // 2. 声明LocationListener类
+        locationListener = new MainActivity.MyLocationListener();
+        // 3. 注册监听函数
+        locationClient.registerLocationListener(locationListener);
+        // 4. 设置参数
+        LocationClientOption locOption = new LocationClientOption();
+        locOption.setLocationMode(LocationClientOption.LocationMode.Hight_Accuracy);// 设置定位模式
+        locOption.setCoorType("bd09ll");// 设置定位结果类型
+        locOption.setScanSpan(5000);// 设置发起定位请求的间隔时间,ms
+        locOption.setIsNeedAddress(true);// 返回的定位结果包含地址信息
+        locOption.setNeedDeviceDirect(true);// 设置返回结果包含手机的方向
+
+        locationClient.setLocOption(locOption);
+        // 5. 注册位置提醒监听事件
+        notifyListener = new MainActivity.MyNotifyListener();
+        notifyListener.SetNotifyLocation(longitude, latitude, 3000, "bd09ll");//精度，维度，范围，坐标类型
+        locationClient.registerNotify(notifyListener);
+        // 6. 开启/关闭 定位SDK
+        locationClient.start();
+
+        }
+    class MyLocationListener implements BDLocationListener {
+        @Override
+        public void onConnectHotSpotMessage(String s, int i) {
+
+        }
+
+        // 异步返回的定位结果
+        @Override
+        public void onReceiveLocation(BDLocation location) {
+            if (location == null) {
+                return;
+            }
+            locType = location.getLocType();
+            longitude = location.getLongitude();
+            latitude = location.getLatitude();
+            Toast.makeText(MainActivity.this, "当前定位的返回值是："+locType+" "+longitude+" "+latitude, Toast.LENGTH_SHORT).show();
+            if (location.hasRadius()) {// 判断是否有定位精度半径
+                radius = location.getRadius();
+            }
+            if (locType == BDLocation.TypeGpsLocation) {//
+                Toast.makeText(
+                        MainActivity.this,
+                        "当前速度是：" + location.getSpeed() + "~~定位使用卫星数量："
+                                + location.getSatelliteNumber(),
+                        Toast.LENGTH_SHORT).show();
+            } else if (locType == BDLocation.TypeNetWorkLocation) {
+                addrStr = location.getAddrStr();// 获取反地理编码(文字描述的地址)
+                Toast.makeText(MainActivity.this, addrStr,
+                        Toast.LENGTH_SHORT).show();
+            }
+            direction = location.getDirection();// 获取手机方向，【0~360°】,手机上面正面朝北为0°
+            province = location.getProvince();// 省份
+            city = location.getCity();// 城市
+            district = location.getDistrict();// 区县
+            Toast.makeText(MainActivity.this,
+                    province + "~" + city + "~" + district, Toast.LENGTH_SHORT)
+                    .show();
+            // 构造定位数据
+            MyLocationData locData = new MyLocationData.Builder()
+                    .accuracy(radius)//
+                    .direction(direction)// 方向
+                    .latitude(latitude)//
+                    .longitude(longitude)//
+                    .build();
+            // 设置定位数据
+            bdMap.setMyLocationData(locData);
+            LatLng ll = new LatLng(latitude, longitude);
+            MapStatusUpdate msu = MapStatusUpdateFactory.newLatLng(ll);
+            bdMap.animateMapStatus(msu);
+
+        }
+    }
+
+    /**
+     * 位置提醒监听器
+     * @author ys
+     *
+     */
+    class MyNotifyListener extends BDNotifyListener {
+        @Override
+        public void onNotify(BDLocation bdLocation, float distance) {
+            super.onNotify(bdLocation, distance);
+            mVibrator.vibrate(1000);//振动提醒已到设定位置附近
+            Toast.makeText(MainActivity.this, "震动提醒", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+        locationClient.unRegisterLocationListener(locationListener);
+        //取消位置提醒
+        locationClient.removeNotifyEvent(notifyListener);
+        locationClient.stop();
+    }
+    //实现按键两次退出
+    //退出时的时间
+    private long mExitTime;
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK && event.getRepeatCount() == 0) {
+
+            exit();
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+    public void exit() {
+        if ((System.currentTimeMillis() - mExitTime) > 2000) {
+            Toast.makeText(MainActivity.this, "再按一次退出LocalVisual", Toast.LENGTH_SHORT).show();
+            mExitTime = System.currentTimeMillis();
+        } else {
+
+            finish();
+            System.exit(0);
+        }
+    }
+
+    }
---
+++ app/src/main/java/com/example/lile/localvisual/SearchResultActivity.java
@@ -0,0 +1,11 @@
+package com.example.lile.localvisual;
+
+import android.app.Activity;
+
+/**
+ * Created by lile on 2017/4/23.
+ * 用于呈现当前的搜索结果的一个页面
+ */
+
+public class SearchResultActivity extends Activity {
+}
